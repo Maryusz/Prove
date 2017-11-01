77031429c8b20f381d1b11c5f4d3ba9b53edacab
Maryusz <mariusz.mab@gmail.com>
2017-11-01 22:45:06 +0100

    Jar Created

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -19,7 +19,12 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+
 
 public class Controller implements Initializable{
 
@@ -55,7 +60,7 @@ public class Controller implements Initializable{
 
         gp = canvas.getGraphicsContext2D();
 
-        gp.setFill(Color.valueOf("#1A3399"));
+        gp.setFill(Color.valueOf("#FFFFFF"));
         gp.fillRect(0,0, canvas.getWidth(), canvas.getHeight());
         gp.stroke();
         pw = gp.getPixelWriter();
@@ -71,6 +76,7 @@ public class Controller implements Initializable{
                     setStartingPoint(event.getX(), event.getY());
                 } else if (MouseEvent.MOUSE_RELEASED == event.getEventType()){
                     setEndingPoint(event.getX(), event.getY());
+                    drawRandomLine();
                 }else if (MouseEvent.MOUSE_DRAGGED == event.getEventType()){
                     drawLine(event.getX(), event.getY());
                 }
@@ -104,15 +110,39 @@ public class Controller implements Initializable{
     }
 
     private Point2D newPoint(Point2D p){
-        int xLoc = random.nextInt(3) - 1;
-        int yLoc = random.nextInt(3) - 1;
+
+        double distance = p.distance(endPoint);
+        Point2D nPoint;
+
+        int xLoc = 0;
+        int yLoc = 0;
+        //Effettua 2 prove per vedere la distanza minore all'endpoint
+        for (int i = 0; i < 2; i++) {
+            xLoc = random.nextInt(3) - 1;
+            yLoc = random.nextInt(3) - 1;
+
+            nPoint = p.add(xLoc, yLoc);
+            if (nPoint.distance(endPoint) < distance) {
+                return nPoint;
+            }
+        }
         return p.add(xLoc, yLoc);
+
     }
 
 
     @FXML
     void updatePb(ActionEvent event) {
+        //TODO: Must generate a shape for automatic construction
+    }
 
+
+    private double checkDistance(Point2D point2D) {
+        return point2D.distance(endPoint);
+
+    }
+
+    private void drawRandomLine() {
         int iterazioni = Integer.parseInt(fieldIterazioni.getText());
 
         Task<Void> t = new Task<Void>() {
@@ -123,23 +153,30 @@ public class Controller implements Initializable{
                     public void run() {
                         Point2D  np = newPoint(startPoint);
                         pw.setColor((int)np.getX(), (int) np.getY(), colorPicker.getValue());
-                        point2DList.add(np);
 
                         for (int i = 0; i < iterazioni; i++){
-
                             np = newPoint(np);
+                            if (checkDistance(np) < 2.0) {
+                                break;
+                            }
+
                             if (checkSovrapposizione.isSelected()) {
-
                                 np = newPoint(np);
-
                             } else {
+                                int count = 0;
+
                                 while (point2DList.contains(np)){
-                                np = newPoint(np);
-                                 }
+                                    np = newPoint(np);
+                                    count++;
+                                    // If theres no other way it breakes the loop.
+                                    if (count > 14) {
+                                        break;
+                                    }
+                                }
                             }
                             point2DList.add(np);
-                            updateProgress(i, iterazioni);
 
+                            updateProgress(i, iterazioni);
                             pw.setColor((int)np.getX(), (int) np.getY(), colorPicker.getValue() );
                         }
                     }
@@ -155,7 +192,6 @@ public class Controller implements Initializable{
 
         pb.progressProperty().bind(t.progressProperty());
         lb.setText(String.format("%d pixel scritti", point2DList.size()));
-
     }
 
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Random Map Designer");
+        primaryStage.setTitle("Random Map Designer - Mariusz Arnold Bilda 2017");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
